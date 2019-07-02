a627cd2ae2353df1a7da3b172cd29ea16cbf1bcf
checktheroads <mtt.lov@gmail.com>
2019-07-01 20:33:26 -0700

    fix packaging

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+import sys
+import shutil
+
+if sys.version_info < (2, 7):
+    sys.exit("Python 2.7+ is required.")
+
+shutil.copyfile("generate_circuit_id.py", "generate-circuit-id")
+
+setup(
+    name="Generate Circuit ID",
+    version="0.0.1",
+    python_requires=">=2.7.0",
+    packages=["generate_circuit_id"],
+    install_requires=["click>=6.7"],
+    license="Do What The F*ck You Want To Public License",
+    long_description=open("README.md").read(),
+    scripts=["generate-circuit-id"],
+)
